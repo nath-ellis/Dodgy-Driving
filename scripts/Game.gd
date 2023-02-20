10b5626205d1bf0617c6b0a69f20c3428c0200f7
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-02-20 17:16:49 +0000

    Created an add_lives() function and moved the code for adding lives into it

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 
 var life_sprite = preload("res://assets/life.png")
-var x = 557
-var y = 48
 
 
-func _ready():
+func add_lives():
+	var x = 557
+	var y = 48
+	
 	# Add correct amount of lives
 	for _i in range($Player.get_meta("lives")):
 		var life = Sprite.new()
@@ -24,6 +25,10 @@ func _ready():
 			y += 70
 
 
+func _ready():
+	add_lives()
+
+
 func _process(_delta):
 	if $Player.get_meta("lives") <= 0:
 		get_tree().change_scene("res://scenes/GameOver.tscn")
@@ -36,22 +41,5 @@ func restart():
 	# Remove leftover lives
 	for l in $Lives.get_children():
 		l.queue_free()
-		
-	# Add lives
-	x = 557
-	y = 48
 	
-	for _i in range($Player.get_meta("lives")):
-		var life = Sprite.new()
-		life.position = Vector2(x, y)
-		life.scale = Vector2(2, 2)
-		life.set_texture(life_sprite)
-		
-		$Lives.add_child(life)
-		
-		# Overflow to the left if the player has enough lives to reach the bottom
-		if y >= get_viewport_rect().size.y - 96:
-			x -= 78
-			y = 48
-		else:
-			y += 70
+	add_lives()
