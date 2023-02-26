6cdbd65eb0cc45e43a968e000f393cc1a1819d90
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-02-26 14:05:22 +0000

    Added a random chance for a group of enemies to block off a lane for a random amount of time

--- scripts/Endless.gd
+++ scripts/Endless.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 
 const life_sprite = preload("res://assets/life.png")
+const enemy = preload("res://scenes/Enemy.tscn")
+
+var lane_blocked = false
+var lane_to_block = 1
+var fixed_enemies = []
+var rand = RandomNumberGenerator.new()
 
 onready var player = $Player
 onready var road = $Road
@@ -32,13 +38,54 @@ func add_lives() -> void:
 			y += 70
 
 
+func block_lane() -> void:
+	var x
+	
+	match lane_to_block:
+		1: x = 120
+		2: x = 239
+		3: x = 361
+		4: x = 480
+	
+	# Add enemies which will stay in the same lane
+	for _i in range(8):
+		var e = enemy.instance()
+		
+		e.position = Vector2(x, e.position.y)
+		e.set_meta("stay_in_lane", true)
+		
+		enemies.append(e)
+		fixed_enemies.append(e)
+		
+		road.add_child(e)
+		
+	lane_blocked = true
+
+
+func open_lane() -> void:
+	for f in fixed_enemies:
+		f.set_meta("stay_in_lane", false)
+	
+	enemies.resize(len(enemies) - len(fixed_enemies))
+	fixed_enemies = []
+	lane_blocked = false
+
+
 func _ready() -> void:
+	rand.randomize()
 	add_lives()
 
 
 func _process(_delta) -> void:
 	if player.get_meta("lives") <= 0:
 		get_tree().change_scene("res://scenes/GameOver.tscn")
+		
+	if rand.randi_range(1, 100) == 5 and !lane_blocked:
+		lane_to_block = rand.randi_range(1, 4)
+		block_lane()
+	
+	if rand.randi_range(1, 100) == 6 and lane_blocked:
+		open_lane()
 
 
 func restart() -> void:
@@ -51,3 +98,4 @@ func restart() -> void:
 		l.queue_free()
 	
 	add_lives()
+	open_lane()
--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -111,48 +111,93 @@ func randomize_enemy() -> void:
 	
 	change_type()
 	
-	var choice = rand.randi_range(1, 2)
-	
-	match choice:
-		1:
-			choice = rand.randi_range(1, 2)
-			
-			match choice:
-				1:
-					position = Vector2(
-						361, 
-						-rand.randi_range(72, 300)
-					)  # Third lane
-				2:
-					position = Vector2(
-						480, 
-						-rand.randi_range(72, 300)
-					)  # Fourth lane
-			
-			sprite.rotation_degrees = 180  # Flip sprite upside down
-			
-			if vel.y < 0:  # If it is moving in the wrong direction
-				vel.y *= -1
-			# TODO: Add a random chance that an enemy will be travelling the wrong way
-		2:
-			choice = rand.randi_range(1, 2)
-			
-			match choice:
-				1:
-					position = Vector2(
-						120, 
-						rand.randi_range(get_viewport_rect().size.y, get_viewport_rect().size.y + 300)
-					)  # First lane
-				2:
-					position = Vector2(
-						239, 
-						rand.randi_range(get_viewport_rect().size.y, get_viewport_rect().size.y + 300)
-					)  # Second lane
-					
-			sprite.rotation_degrees = 0
-			
-			if vel.y > 0:  # If it is moving in the wrong direction
-				vel.y *= -1
+	if has_meta("stay_in_lane"):  # Keep the enemy in the same lane
+		if get_meta("stay_in_lane"):
+			if position.x <= 206:
+				position = Vector2(
+					120, 
+					rand.randi_range(get_viewport_rect().size.y, get_viewport_rect().size.y + 900)
+				) 
+				
+				sprite.rotation_degrees = 0
+				
+				if vel.y > 0:  # If it is moving in the wrong direction
+					vel.y *= -1
+			elif position.x <= 329:
+				position = Vector2(
+					239, 
+					rand.randi_range(get_viewport_rect().size.y, get_viewport_rect().size.y + 900)
+				) 
+				
+				sprite.rotation_degrees = 0
+				
+				if vel.y > 0:  # If it is moving in the wrong direction
+					vel.y *= -1
+			elif position.x <= 452:
+				position = Vector2(
+					361, 
+					-rand.randi_range(72, 900)
+				)
+				
+				sprite.rotation_degrees = 180  # Flip sprite upside down
+				
+				if vel.y < 0:  # If it is moving in the wrong direction
+					vel.y *= -1
+			else:
+				position = Vector2(
+					480, 
+					-rand.randi_range(72, 900)
+				)
+				
+				sprite.rotation_degrees = 180  # Flip sprite upside down
+				
+				if vel.y < 0:  # If it is moving in the wrong direction
+					vel.y *= -1
+		else:
+			queue_free()
+	else:  # Randomize it if it is not meant to stay in a lane
+		var choice = rand.randi_range(1, 2)
+		
+		match choice:
+			1:
+				choice = rand.randi_range(1, 2)
+				
+				match choice:
+					1:
+						position = Vector2(
+							361, 
+							-rand.randi_range(72, 300)
+						)  # Third lane
+					2:
+						position = Vector2(
+							480, 
+							-rand.randi_range(72, 300)
+						)  # Fourth lane
+				
+				sprite.rotation_degrees = 180  # Flip sprite upside down
+				
+				if vel.y < 0:  # If it is moving in the wrong direction
+					vel.y *= -1
+				# TODO: Add a random chance that an enemy will be travelling the wrong way
+			2:
+				choice = rand.randi_range(1, 2)
+				
+				match choice:
+					1:
+						position = Vector2(
+							120, 
+							rand.randi_range(get_viewport_rect().size.y, get_viewport_rect().size.y + 300)
+						)  # First lane
+					2:
+						position = Vector2(
+							239, 
+							rand.randi_range(get_viewport_rect().size.y, get_viewport_rect().size.y + 300)
+						)  # Second lane
+						
+				sprite.rotation_degrees = 0
+				
+				if vel.y > 0:  # If it is moving in the wrong direction
+					vel.y *= -1
 
 
 func collision() -> void:
