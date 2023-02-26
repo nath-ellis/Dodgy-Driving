b9a0082247680ea095ac0f5ba8b42603c4f67820
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-02-26 10:00:46 +0000

    Added a way to select the mode when the play button is pressed

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -2,8 +2,14 @@ extends Node2D
 
 
 onready var play_btn = $UI/Play
+onready var endless_btn = $UI/Modes/Endless
+onready var modes = $UI/Modes
 
 
 func _process(_delta) -> void:
 	if play_btn.pressed:
+		modes.show()
+		play_btn.hide()
+	
+	if endless_btn.pressed:
 		get_tree().change_scene("res://scenes/Endless.tscn")
