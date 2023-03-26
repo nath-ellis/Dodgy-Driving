135ce062a5f3a777fe592f3db2190b8434274d9c
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-03-26 15:38:16 +0100

    Added a menu button to the game over scene

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -11,3 +11,7 @@ func _on_EnemyTimer_timeout() -> void:
 
 func _on_play_again_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Endless.tscn")
+
+
+func _on_menu_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Menu.tscn")
